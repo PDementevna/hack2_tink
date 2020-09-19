6c7dab6e8ccb86c310183990cb29806ebf180706
ilinvalery <valery.ilin@skoltech.ru>
2020-09-19 11:03:10 +0300

    widget page final

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import './App.css'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+
 import phone from "./IPhone_X_vector.svg"
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {
+    faCircle,
+    faMicrophone,
+    faQuestionCircle,
+    faSearch
+} from '@fortawesome/free-solid-svg-icons'
+
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import Button from "./Components/EmptyButton";
+import WidgetPage from "./Components/WidgetPage";
+
+
+library.add(faMicrophone, faSearch, faCircle, faQuestionCircle)
 
 
 function App() {
     return (
         <div className="wrapper">
-
             <div className="sub-wrapper">
                 <img src={phone} className={"image-back"} alt={"mobile"}/>
-                <Button>Text</Button>
-                <Button>Text</Button>
-                <Button>Text</Button>
-                <Button>Text</Button>
+                <div className="phone-wrapper">
+                    <Router>
+                        <Switch>
+                            <Route path="/">
+                                <WidgetPage/>
+                            </Route>
+                            {/*more routers here + TODO 404 page*/}
+                        </Switch>
+                    </Router>
+                </div>
             </div>
         </div>
     );
