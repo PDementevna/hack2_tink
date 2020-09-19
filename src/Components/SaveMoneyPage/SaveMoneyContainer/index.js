546ac2b3d68e96b3a19ba33263a70cbde16d14f7
ilinvalery <valery.ilin@skoltech.ru>
2020-09-19 16:47:13 +0300

    fix save

--- src/Components/SaveMoneyPage/SaveMoneyContainer/index.js
+++ src/Components/SaveMoneyPage/SaveMoneyContainer/index.js
@@ -18,7 +18,7 @@ export default class SaveMoneyContainer extends React.Component {
             <div className={"save-money-container"}>
                 <h2 className={"save-money-caption"}>{this.props.data.title}</h2>
                 <img className={"save-money"} src={this.props.data.image} alt={this.props.data.shop}/>
-                <p className={"save-money"}>Можно<br/>сэконоить</p>
+                <p className={"save-money"}>Можно<br/>сэкономить</p>
                 <h3 className={"save-money-amount"}>{this.props.data.amount}₽</h3>
                 <h3 className={"save-money-shop"}>{this.props.data.shop}</h3>
             </div>
