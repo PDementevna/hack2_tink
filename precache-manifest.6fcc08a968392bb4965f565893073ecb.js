96a663a7831b9968d2608ee6dafbc2b51757adb7
ilinvalery <valery.ilin@skoltech.ru>
2020-09-19 11:35:42 +0300

    Updates

---
+++ precache-manifest.6fcc08a968392bb4965f565893073ecb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e963caae020c7748a357b6076df16fbe",
+    "url": "/hack2_tink/index.html"
+  },
+  {
+    "revision": "ebdce84014e9b41380d2",
+    "url": "/hack2_tink/static/css/main.a667b30b.chunk.css"
+  },
+  {
+    "revision": "45313269447eb942382b",
+    "url": "/hack2_tink/static/js/2.295537f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hack2_tink/static/js/2.295537f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebdce84014e9b41380d2",
+    "url": "/hack2_tink/static/js/main.20fa787b.chunk.js"
+  },
+  {
+    "revision": "9c6120a3701e07a6af4d",
+    "url": "/hack2_tink/static/js/runtime-main.418ca1a0.js"
+  },
+  {
+    "revision": "5afa0f319487be9fd037e0dd33836569",
+    "url": "/hack2_tink/static/media/IPhone_X_vector.5afa0f31.svg"
+  }
+]);
